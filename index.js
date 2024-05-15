48373b3ad4f1eced76352b1d652fac9cdf166d2a
HuzaiFa <167712059+HuzaiFaRz@users.noreply.github.com>
2024-05-15 23:41:52 +0500

    some

--- index.js
+++ index.js
@@ -5,3 +5,42 @@ function navbarMenu() {
   });
 }
 navbarMenu();
+const myToastEl = document.getElementById('liveToastBtn')
+myToastEl.addEventListener('hidden.bs.toast', () => {
+  // do something...
+})
+ 
+
+
+// var offCanvasChatSearchInput = document.querySelector(
+//   ".chat-offcanvas-form-input"
+// );
+// var offCanvasChatSearchIcon = document.querySelector(
+//   ".chat-offcanvas-form-icon"
+// );
+
+// var offCanvasChatContactName = document.querySelectorAll(".chat-contact-name");
+
+// function offCanvasChatSearch() {
+//   offCanvasChatSearchIcon.addEventListener("click", function (a) {
+//     a.preventDefault();
+//     var offCanvasChatSearchInputValue =
+//       offCanvasChatSearchInput.value.toUpperCase();
+//     console.log(offCanvasChatSearchInputValue);
+
+//     offCanvasChatSearchInput = offCanvasChatSearchInput.value;
+//     console.log(offCanvasChatSearchInput);
+//     offCanvasChatSearchInput.value = "";
+
+//     for (var i = 0; i < offCanvasChatContactName.length; i++) {
+//       if (
+//         offCanvasChatSearchInputValue === offCanvasChatContactName[i].innerHTML
+//       ) {
+//         console.log("value same");
+//       } else {
+//         console.log("not same");
+//       }
+//     }
+//   });
+// }
+// offCanvasChatSearch();
