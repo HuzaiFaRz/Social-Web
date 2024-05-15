19b7d6ea958d3ce2f1ca07c67e20f0c9c19710c0
HuzaiFa <167712059+HuzaiFaRz@users.noreply.github.com>
2024-05-15 13:57:15 +0500

    123

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var navbarMenuLinesButton = document.querySelector(".menu-lines");
-navbarMenuLinesButton.addEventListener("click", function () {
-  navbarMenuLinesButton.classList.toggle("navbarMenuLinesButtonActive");
-});
+var navbarMenuLines = document.querySelector(".menu-lines");
+function navbarMenu() {
+  navbarMenuLines.addEventListener("click", function () {
+    navbarMenuLines.classList.toggle("navbarMenuLinesActive");
+  });
+}
+navbarMenu();
