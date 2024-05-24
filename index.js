e34e5370334754cf82ebb055fa59ae43d6cf6704
HuzaiFa <167712059+HuzaiFaRz@users.noreply.github.com>
2024-05-25 04:34:35 +0500

    some

--- index.js
+++ index.js
@@ -422,3 +422,5 @@
 //     reloadContainerAnimation();
 //   },6000);
 // });
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
